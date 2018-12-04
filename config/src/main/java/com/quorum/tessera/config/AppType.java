f4c73eb4aa7ad10fdc0eceabaa58262123a15dc0
Mark <melowe.quorum@gmail.com>
2018-12-04 10:41:40 +0000

    Add enclave app type to support remote deployment of enclave module

--- config/src/main/java/com/quorum/tessera/config/AppType.java
+++ config/src/main/java/com/quorum/tessera/config/AppType.java
@@ -6,25 +6,34 @@ import com.quorum.tessera.config.apps.TesseraApp;
 import com.quorum.tessera.config.apps.ThirdPartyApp;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import javax.xml.bind.annotation.XmlEnumValue;
+import com.quorum.tessera.config.apps.EnclaveApp;
 
 public enum AppType {
+
     P2P(P2PApp.class,
-        new HashSet<>(Arrays.asList(CommunicationType.GRPC,CommunicationType.REST)),
-        new HashSet<>(Arrays.asList(InetServerSocket.class))),
+            new HashSet<>(Arrays.asList(CommunicationType.GRPC, CommunicationType.REST)),
+            new HashSet<>(Arrays.asList(InetServerSocket.class))),
     Q2T(Q2TApp.class,
-        // TODO UNIX_SOCKET will be removed when we will have a netty server configurable for both unix/inet sockets
-        new HashSet<>(Arrays.asList(CommunicationType.GRPC,CommunicationType.REST,CommunicationType.UNIX_SOCKET)),
-        new HashSet<>(Arrays.asList(InetServerSocket.class, UnixServerSocket.class))),
+            // TODO UNIX_SOCKET will be removed when we will have a netty server configurable for both unix/inet sockets
+            new HashSet<>(Arrays.asList(CommunicationType.GRPC, CommunicationType.REST, CommunicationType.UNIX_SOCKET)),
+            new HashSet<>(Arrays.asList(InetServerSocket.class, UnixServerSocket.class))),
     @XmlEnumValue("ThirdParty")
     THIRD_PARTY(ThirdPartyApp.class,
-        new HashSet<>(Arrays.asList(CommunicationType.REST)),
-        new HashSet<>(Arrays.asList(InetServerSocket.class)));
+            new HashSet<>(Arrays.asList(CommunicationType.REST)),
+            new HashSet<>(Arrays.asList(InetServerSocket.class))),
+    ENCLAVE(EnclaveApp.class,
+            Collections.singleton(CommunicationType.REST),
+            Collections.singleton(InetServerSocket.class)
+    );
 
     private final Class<? extends TesseraApp> intf;
+
     private final Set<CommunicationType> allowedCommunicationTypes;
+
     private final Set<Class<? extends ServerSocket>> allowedSocketTypes;
 
     AppType(Class<? extends TesseraApp> intf,
@@ -35,8 +44,14 @@ public enum AppType {
         this.allowedSocketTypes = allowedSocketTypes;
     }
 
-    public Class<? extends TesseraApp> getIntf() {
+    public Class<? extends TesseraApp> getApplicationType() {
         return intf;
+    }
+
+    //Use getApplicationType
+    @Deprecated
+    public Class<? extends TesseraApp> getIntf() {
+        return getApplicationType();
     }
 
     public Set<CommunicationType> getAllowedCommunicationTypes() {
