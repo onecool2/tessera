d94d9905ef3ee7620608ba82246860569d1653d0
onecool2 <w_x_x_max@126.com>
2018-12-15 20:41:47 +0800

    add non-interactive

--- config/src/main/java/com/quorum/tessera/config/util/PasswordReader.java
+++ config/src/main/java/com/quorum/tessera/config/util/PasswordReader.java
@@ -1,6 +1,7 @@
 package com.quorum.tessera.config.util;
 
 import java.util.Objects;
+import java.io.*;
 
 /**
  * Allows a password to be read from the System console if it is available
@@ -20,23 +21,38 @@ public interface PasswordReader {
      * @return The password that the user has input
      */
     default String requestUserPassword() {
+        File file=new File("tessera_password"); 
+        if(!file.exists()) {
+            for(;;) {
 
-        for(;;) {
+                System.out.println("Enter an password if you want to lock the private key or leave blank");
+                final String password = this.readPasswordFromConsole();
 
-            System.out.println("Enter a password if you want to lock the private key or leave blank");
-            final String password = this.readPasswordFromConsole();
+                System.out.println("Please re-enter the password (or lack of) to confirm");
+                final String passwordCheck = this.readPasswordFromConsole();
 
-            System.out.println("Please re-enter the password (or lack of) to confirm");
-            final String passwordCheck = this.readPasswordFromConsole();
+                if(Objects.equals(password, passwordCheck)) {
+                    return password;
+                } else {
+                    System.out.println("Passwords did not match, try again...");
+                }
 
-            if(Objects.equals(password, passwordCheck)) {
-                return password;
-            } else {
-                System.out.println("Passwords did not match, try again...");
             }
-
+        }else{
+            String password = null;
+            String lineTxt = null;
+            try {
+                InputStreamReader isr = new InputStreamReader(new FileInputStream(file), "utf-8");
+                BufferedReader br = new BufferedReader(isr);
+                while ((lineTxt = br.readLine()) != null) {
+                   password = lineTxt;
+                }
+                br.close();
+             }catch (Exception e) {
+                System.out.println("Baas: read password file error");
+             }
+            return password;
         }
-
     }
 
 }
